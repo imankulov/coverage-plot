2dc786fb353c3b2c1c7c4dfe11d013fe9d85c981
Roman Imankulov <roman.imankulov@gmail.com>
2022-04-12 09:50:21 +0100

    Bump version: 0.1.0 → 0.2.0

--- coverage_plot/__init__.py
+++ coverage_plot/__init__.py
@@ -1,4 +1,4 @@
 from coverage_plot.plot import import_json, import_xml, plot_sunburst, plot_treemap
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ["import_json", "import_xml", "plot_sunburst", "plot_treemap", "__version__"]
